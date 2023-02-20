d730863e2e67ecd747de9aaf355e483f63dc2b95
Ericsson Tribess <ericktribess@gmail.com>
2023-02-20 13:37:35 -0300

    first commit

---
+++ src/main/java/com/example/m1s02/M1s02Application.java
@@ -0,0 +1,13 @@
+package com.example.m1s02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1s02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1s02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m1s02/M1s02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m1s02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1s02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
